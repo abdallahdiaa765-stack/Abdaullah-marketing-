8e3fc856b5a6b3fd8dd0319f20f40c4d2644cef1
abdallahmarketingbusiness-a11y <abdallahmarketing.business@gmail.com>
2026-06-07 11:09:33 +0300

    Create layout.jsx

---
+++ app/layout.jsx
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: 'Abdullah Marketing',
+  description: 'Social Media Marketing',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="ar">
+      <body>{children}</body>
+    </html>
+  )
+}
